baefe0931cef5262f6bd5105980303319572ba58
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-10 10:54:57 -0400

    Display Project List items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-  const navLinks = links.map((link) => <a href= "#{link}" key = {link}>{link}</a>)
+  const navLinks = links.map((link) => <a href= {link} key={link}>{link}</a>)
 
   return <nav>{navLinks}</nav>;
 }
