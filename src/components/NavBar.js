e74c73e34a7dc194c52d2b5564c55099b0a6c27a
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-10 11:00:10 -0400

    Fixed links to pass test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ["#home", "#about", "#projects"];
   const navLinks = links.map((link) => <a href= {link} key={link}>{link}</a>)
 
   return <nav>{navLinks}</nav>;
